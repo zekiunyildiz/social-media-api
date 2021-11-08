02aea45c694ff7b46a3a8c59cb64cccfb858d483
zekiunyildiz <zekiunyildiz@gmail.com>
2021-11-08 16:34:02 +0300

    Middlewares and First Request

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'))
 
+app.get("/",(req, res) => {
+  res.send('Welcome to Homepage')
+})
+
+app.get("/users",(req, res) => {
+  res.send('Welcome to User page')
+})
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
